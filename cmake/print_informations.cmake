97ab4eb16fa1f639f863640bfe7cb759796d2d77
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-05-24 13:08:08 -0400

    chore: Perform the CMake prints

--- cmake/print_informations.cmake
+++ cmake/print_informations.cmake
@@ -2,14 +2,14 @@
 
 ## project info
 message(STATUS "")
-message(STATUS "Project info.:")
+message(STATUS "Project info.")
 message(STATUS "PROJECT_NAME:                 ${PROJECT_NAME}")
 message(STATUS "PROJECT_FULL_NAME:            ${PROJECT_FULL_NAME}")
 message(STATUS "PACKAGE_VERSION:              ${PACKAGE_VERSION}")
 
 ## Options info
 message(STATUS "")
-message(STATUS "Options info.:")
+message(STATUS "Options info.")
 message(STATUS "TESTING:                      ${${PROJECT_NAME}_TESTING}")
 message(STATUS "CONAN:                        ${${PROJECT_NAME}_CONAN}")
 message(STATUS "DOXYGEN:                      ${${PROJECT_NAME}_DOXYGEN}")
@@ -20,7 +20,7 @@ message(STATUS "VOID_PREFIX:                  ${${PROJECT_NAME}_VOID_PREFIX}")
 
 ## cmake info
 message(STATUS "")
-message(STATUS "CMake info.:")
+message(STATUS "CMake info.")
 message(STATUS "CMAKE_INSTALL_PREFIX:         ${CMAKE_INSTALL_PREFIX}")
 message(STATUS "PROJECT_DATADIR:              ${${PROJECT_NAME}_DATADIR}")
 message(STATUS "CMAKE_INSTALL_LIBDIR:         ${CMAKE_INSTALL_LIBDIR}")
@@ -36,7 +36,7 @@ message(STATUS "CMAKE_PREFIX_PATH:            ${CMAKE_PREFIX_PATH}")
 ## Win32 info
 if(WIN32)
 	message(STATUS "")
-	message(STATUS "Win32 info.:")
+	message(STATUS "Win32 info.")
 	message(STATUS "HAVE_MINGW64:             ${HAVE_MINGW64}")
 	message(STATUS "MINGW_PATH:               ${MINGW_PATH}")
 	message(STATUS "MINGW_ARCH:               ${MINGW_ARCH}")
