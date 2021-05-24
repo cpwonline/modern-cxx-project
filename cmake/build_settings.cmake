d5d5a50d6e958e9adbbddbfef00e334ffbebef0c
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-05-24 12:43:31 -0400

    chore: New perform for build settings
    Export compile commands is On.
    
    Change sentences and conditionals for
    the installation prefix.

--- cmake/build_settings.cmake
+++ cmake/build_settings.cmake
@@ -1,8 +1,8 @@
 # Build security
 if(PROJECT_SOURCE_DIR STREQUAL PROJECT_BINARY_DIR)
-  message(
-    FATAL_ERROR "In-source builds not allowed. Please make a new directory (called a build directory) and run CMake from there."
-  )
+	message(
+		FATAL_ERROR "In-source builds not allowed. Please make a new directory (called a build directory) and run CMake from there."
+	)
 endif()
 
 # Language standard
@@ -15,20 +15,16 @@ if(NOT CMAKE_BUILD_TYPE)
 endif()
 
 # Compiler options
-set(CMAKE_CXX_FLAGS_DEBUG "-g -O0  -Wall -Wextra")
+set(CMAKE_CXX_FLAGS_DEBUG "-g -O0 -Wall -Wextra")
 set(CMAKE_CXX_FLAGS_RELEASE "-O3")
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 # Set up prefix
-if(_VOID_PREFIX)
-	set(CMAKE_INSTALL_PREFIX "" CACHE PATH "..." FORCE)
-	set(${PROJECT_NAME}_DATADIR "share")
+if(${PROJECT_NAME}_VOID_PREFIX)
+	set(CMAKE_INSTALL_PREFIX ${PROJECT_BINARY_DIR} CACHE PATH "..." FORCE)
 else()
-	if(_ENABLE_INSTALL_TO_TMP)
-		set(CMAKE_INSTALL_PREFIX "/tmp/${PROJECT_NAME}" CACHE PATH "..." FORCE)
-	else()
-		if(NOT CMAKE_INSTALL_PREFIX)
-			set(CMAKE_INSTALL_PREFIX "/usr/local" CACHE PATH "..." FORCE)
-		endif()
+	if(NOT CMAKE_INSTALL_PREFIX)
+		set(CMAKE_INSTALL_PREFIX "/usr/local" CACHE PATH "..." FORCE)
 	endif()
-	set(${PROJECT_NAME}_DATADIR "${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}")
 endif()
+set(${PROJECT_NAME}_DATADIR "${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}")
