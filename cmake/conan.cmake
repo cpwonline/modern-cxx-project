10346266312743da86f3c60b86430d52241daedb
Jose F Rivas C <josefelixrc7@gmail.com>
2021-06-02 22:13:21 -0400

    chore: Add a subdirectory for Conan files

--- cmake/conan.cmake
+++ cmake/conan.cmake
@@ -1,16 +1,16 @@
 if(${PROJECT_NAME}_CONAN)
-	list(APPEND CMAKE_MODULE_PATH ${PROJECT_BINARY_DIR})
-	list(APPEND CMAKE_PREFIX_PATH ${PROJECT_BINARY_DIR})
-	if(NOT EXISTS "${PROJECT_BINARY_DIR}/conan.cmake")
+	list(APPEND CMAKE_MODULE_PATH ${PROJECT_BINARY_DIR}/conan)
+	list(APPEND CMAKE_PREFIX_PATH ${PROJECT_BINARY_DIR}/conan)
+	if(NOT EXISTS "${PROJECT_BINARY_DIR}/conan/conan.cmake")
 		message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
 		file(
 			DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/v0.16.1/conan.cmake"
-			"${PROJECT_BINARY_DIR}/conan.cmake"
+			"${PROJECT_BINARY_DIR}/conan/conan.cmake"
 			TLS_VERIFY ON
 		)
 	endif()
 
-	include(${PROJECT_BINARY_DIR}/conan.cmake)
+	include(${PROJECT_BINARY_DIR}/conan/conan.cmake)
 
 	conan_cmake_configure(
 		REQUIRES poco/1.9.4
@@ -24,6 +24,7 @@ if(${PROJECT_NAME}_CONAN)
 		BUILD missing
 		REMOTE conan-center
 		SETTINGS ${settings}
+		INSTALL_FOLDER ${PROJECT_BINARY_DIR}/conan
 	)
 
 	message("Conan OK")
